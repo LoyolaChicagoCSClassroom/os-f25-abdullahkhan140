67f85ce8869cfd4f0e92f40283a1dd8fdf014921
Abdullah Khan <120227860+abdullahkhan140@users.noreply.github.com>
2025-09-25 16:28:11 -0500

    Update rprintf.h

--- src/rprintf.h
+++ src/rprintf.h
@@ -1,9 +1,6 @@
-#include "rprintf.h"
+#ifndef RPRINTF_H
+#define RPRINTF_H
 
-void kernel_main(void) {
-    rprintf("Hello, bare-metal world!\n");
-    rprintf("Decimal: %d\n", 1234);
-    rprintf("Hex: %x\n", 0xABCD);
-    rprintf("String: %s\n", "sample");
-    rprintf("Char: %c\n", 'X');
-}
+void rprintf(const char *str);
+
+#endif
