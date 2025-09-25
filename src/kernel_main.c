c57da1381fccba4f2fd63e3f85d5c597042bffde
Abdullah Khan <120227860+abdullahkhan140@users.noreply.github.com>
2025-09-25 15:07:49 -0500

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,4 +1,16 @@
 #include "rprintf.h"
+#include <stdint.h>
+
+#define MULTIBOOT_HEADER_MAGIC    0x1BADB002
+#define MULTIBOOT_HEADER_FLAGS    0x00000003
+#define MULTIBOOT_CHECKSUM        -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+__attribute__((section(".multiboot"))) 
+const uint32_t multiboot_header[] = {
+    MULTIBOOT_HEADER_MAGIC,
+    MULTIBOOT_HEADER_FLAGS,
+    MULTIBOOT_CHECKSUM
+};
 
 void kernel_main(void) {
     rprintf("Hello from kernel!\n");
