646d482d28b34f76a24f6b4bdd52811e27e1c826
Abdullah Khan <120227860+abdullahkhan140@users.noreply.github.com>
2025-09-25 16:27:14 -0500

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,9 +1,10 @@
 #include "rprintf.h"
 
 void kernel_main(void) {
-    rprintf("Hello, bare-metal world!\n");
-    rprintf("Decimal: %d\n", 1234);
-    rprintf("Hex: %x\n", 0xABCD);
-    rprintf("String: %s\n", "sample");
-    rprintf("Char: %c\n", 'X');
+    rprintf("line 1\n");
+    rprintf("line 2\n");
+    rprintf("line 3\n");
+    rprintf("line 4\n");
+    rprintf("line 5\n");
+    while(1) {} // keep kernel running
 }
