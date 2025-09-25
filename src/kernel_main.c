d002af6d2b1f35bd311bdfd1a2ff805a84d50c39
Abdullah Khan <120227860+abdullahkhan140@users.noreply.github.com>
2025-09-25 15:17:19 -0500

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,19 +1,6 @@
 #include "rprintf.h"
-#include <stdint.h>
-
-#define MULTIBOOT_HEADER_MAGIC    0x1BADB002
-#define MULTIBOOT_HEADER_FLAGS    0x00000003
-#define MULTIBOOT_CHECKSUM        -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
-
-__attribute__((section(".multiboot"))) 
-const uint32_t multiboot_header[] = {
-    MULTIBOOT_HEADER_MAGIC,
-    MULTIBOOT_HEADER_FLAGS,
-    MULTIBOOT_CHECKSUM
-};
 
 void kernel_main(void) {
     rprintf("Hello from kernel!\n");
-    rprintf("Number: %d  Hex: %x  String: %s  Char: %c\n",
-            1234, 0xBEEF, "test", 'A');
+    rprintf("Number: %d  Hex: %x  String: %s  Char: %c\n", 1234, 0xBEEF, "test", 'A');
 }
