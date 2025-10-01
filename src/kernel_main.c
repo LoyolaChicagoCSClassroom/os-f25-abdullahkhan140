b58ca25ab1ab51b27491905f6d907b9247e1c2ed
Abdullah Khan <120227860+abdullahkhan140@users.noreply.github.com>
2025-10-01 18:12:39 -0500

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,10 +1,1 @@
-#include "rprintf.h"
 
-void kernel_main(void) {
-    rprintf("line 1\n");
-    rprintf("line 2\n");
-    rprintf("line 3\n");
-    rprintf("line 4\n");
-    rprintf("line 5\n");
-    while(1) {} // keep kernel running
-}
