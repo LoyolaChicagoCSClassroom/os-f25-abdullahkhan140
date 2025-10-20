aee3ec5f434178374bb46a7ea455f3ea98ba929c
Abdullah Khan <120227860+abdullahkhan140@users.noreply.github.com>
2025-10-20 18:51:47 -0500

    Create kernel_main.c

---
+++ hw3/kernel_main.c
@@ -0,0 +1,32 @@
+#include "page.h"
+#include <stdio.h>
+
+int main(void) {
+    printf("Initializing Page Frame Allocator...\n");
+    init_pfa_list();
+
+    printf("Allocating 5 pages...\n");
+    struct ppage *alloc = allocate_physical_pages(5);
+
+    printf("Allocated pages:\n");
+    struct ppage *cur = alloc;
+    while (cur) {
+        printf("Page at physical address: %p\n", cur->physical_addr);
+        cur = cur->next;
+    }
+
+    printf("Freeing allocated pages...\n");
+    free_physical_pages(alloc);
+
+    // Check free list
+    printf("Free list after freeing pages:\n");
+    cur = free_physical_pages;
+    int count = 0;
+    while (cur) {
+        count++;
+        cur = cur->next;
+    }
+    printf("Total free pages: %d\n", count);
+
+    return 0;
+}
