04747fb867caf03bf5c55478b4d58827b549b6a5
Abdullah Khan <120227860+abdullahkhan140@users.noreply.github.com>
2025-10-22 18:10:32 -0500

    Add Multiboot2 header definition

--- hw2/kernel_main.c
+++ hw2/kernel_main.c
@@ -1,3 +1,13 @@
+#define MULTIBOOT2_HEADER_MAGIC 0xe85250d6
+__attribute__((section(".multiboot"), used))
+const unsigned int multiboot_header[] = {
+    0xe85250d6,     
+    0,              
+    24,           
+    0x11ADAF12,     
+     0, 0, 8
+};
+
 #include <stdint.h>
 #include "rprintf.h"
 #include "interrupt.h"
